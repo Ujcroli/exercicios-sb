829790a81071cfab33041f053b9eb9ad92b0fe3d
ujcroli <jonathan.carlos@hotmail.com>
2020-10-16 14:23:06 -0300

    413. passando parâmetros para web

--- src/main/java/br/com/jonathan/exerciciossb/controllers/ClienteController.java
+++ src/main/java/br/com/jonathan/exerciciossb/controllers/ClienteController.java
@@ -1,7 +1,9 @@
 package br.com.jonathan.exerciciossb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.jonathan.exerciciossb.models.Cliente;
@@ -15,4 +17,16 @@ public class ClienteController {
 		return new Cliente(28, "Pedro", "123.456.789-00");
 	}
 
+	@GetMapping("/{id}")
+	public Cliente obterClientePorId1(@PathVariable int id) {
+		return new Cliente(id, "Maria", "987.654.321-00");
+	}
+	
+	
+	@GetMapping
+	public Cliente obterClientePorId2(@RequestParam(name = "id", defaultValue = "1") int id) {
+		return new Cliente(id, "João Agusto", "111.222.333-44");
+	}
+	
+	
 }
